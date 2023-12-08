65335a7a73740c9ced6663ba958fd5b3141244e8
David Brazda <davidbrazda61@gmail.com>
2023-12-08 17:19:39 +0100

    update

--- mlroom/arch/__init__.py
+++ mlroom/arch/__init__.py
@@ -1,6 +1,9 @@
 import os
 
-for filename in os.listdir("mlroom/arch"):
+# Get the directory of the current file (__init__.py)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+for filename in os.listdir(current_dir):
     if filename.endswith(".py") and filename != "__init__.py":
        # __import__(filename[:-3])
         __import__(f"mlroom.arch.{filename[:-3]}")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlroom',
-    version='0.32',
+    version='0.33',
     packages=find_packages(),
     include_package_data=True,
     package_data={
