9a6b23fbe1a07ef7ad6a8ebd581769808b636cd6
David Brazda <davidbrazda61@gmail.com>
2023-12-26 18:17:28 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlroom',
-    version='0.397',
+    version='0.400',
     packages=find_packages(),
     include_package_data=True,
     package_data={
